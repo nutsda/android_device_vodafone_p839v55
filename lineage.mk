04b89ff6ae1a9c5a4b475571ef716d9447bab3ba
John Bennett <nutsda@gmail.com>
2017-11-04 11:08:46 +1300

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff
+# Inherit some common LineageOS stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -15,3 +15,7 @@ PRODUCT_MANUFACTURER := Vodafone
 PRODUCT_RELEASE_NAME := Smart Ultra 6
 
 PRODUCT_GMS_CLIENTID_BASE := android-zte
+
+TARGET_VENDOR_PRODUCT_NAME := P839V55
+TARGET_VENDOR_DEVICE_NAME := P839V55
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=P839V55 PRODUCT_NAME=P839V55
